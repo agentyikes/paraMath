46691bb0a7d8fbf2cc688463456fc8e8e1b1cf62
agentyikes <agentyikes@gmail.cccccom>
2023-03-22 10:35:20 +0800

    layout added
    Signed-off-by: agentyikes <agentyikes@gmail.cccccom>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
-// Step 1: Import React
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to paraMath site!</h1>
-      <Link to="/about">About </Link>
-      <p>Here you can find the parnormal power of Mathemattics.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>paraMath-Home Page</title>
+export const Head = () => <title>Home Page</title>
 
-// Step 3: Export your component
 export default IndexPage
